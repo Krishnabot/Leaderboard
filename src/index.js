c7e39d597c956d59998faaa07b16aef1f61349ca
Krishna <libraryismyparadise@gmail.com>
2022-07-29 18:46:50 +0545

    Add styling to BackGround

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './style.css';
 import AddScore from "./modules/AddScore.js";
 import getScore from "./modules/getScore.js";
+import matrix from "./modules/background-canvas.js";
 
-const form = document.querySelector('#form');
-const name = document.querySelector('#name');
-const score = document.querySelector('#score');
-const LeaderBoard = document.querySelector('#leaderboard');
-const RefreshBtn = document.querySelector('#refresh');
-const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fLLGhFOYzdFIIisID8YZ/scores/';
+const form = document.querySelector("#form");
+const name = document.querySelector("#name");
+const score = document.querySelector("#score");
+const LeaderBoard = document.querySelector("#leaderboard");
+const RefreshBtn = document.querySelector("#refresh");
+const URL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fLLGhFOYzdFIIisID8YZ/scores/";
 
 getScore();
-
+matrix();
 RefreshBtn.addEventListener('click', () => {
   LeaderBoard.innerHTML = '';
   getScore();
---
+++ src/modules/background-canvas.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector(".matrix");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+let cols = Math.floor(window.innerWidth / 20) + 1;
+let ypos = Array(cols).fill(0);
+
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function matrix() {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
+  if (canvas.width !== w) {
+    canvas.width = w;
+    cols = Math.floor(window.innerWidth / 20) + 1;
+    ypos = Array(cols).fill(0);
+  }
+  if (canvas.height !== h) {
+    canvas.height = h;
+  }
+
+  ctx.fillStyle = "#0001";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.fillStyle = "#0f0";
+  ctx.font = "15pt monospace";
+
+  ypos.forEach((y, ind) => {
+    const text = String.fromCharCode(Math.random() * 128);
+    const x = ind * 20;
+    ctx.fillText(text, x, y);
+    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    else ypos[ind] = y + 20;
+  });
+}
+
+setInterval(matrix, 50);
+
+export default matrix;
